2425a2633a3f428f21f0825e284a845c4cc5a57d
Jasa95 <jaco3020@stud.kea.dk>
2024-04-30 11:27:39 +0200

    CM-68 security and auth for navbar

---
+++ src/security/AuthProvider.tsx
@@ -0,0 +1,54 @@
+import { createContext, useState, ReactNode } from 'react';
+import authProvider, { User } from '../services/authFacade';
+import { useContext } from 'react';
+import { LoginReponse, LoginRequest } from '../services/authFacade';
+
+interface AuthContextType {
+    email: string | null;
+    signIn: (email: User) => Promise<LoginReponse>;
+    signOut: () => void;
+    isLoggedIn: () => boolean;
+    isLoggedInAs: (role: string[]) => boolean;
+}
+
+const AuthContext = createContext<AuthContextType>(null!);
+
+export default function AuthProvider({ children }: { children: ReactNode }) {
+    const intialEmail = localStorage.getItem("email") || null;
+    const [email, setEmail] = useState<string | null>(intialEmail);
+
+    const signIn = async (user_: LoginRequest) => {
+        return authProvider.signIn(user_).then((user) => {
+            setEmail(user.email);
+            localStorage.setItem("email", user.email);
+            localStorage.setItem("role", JSON.stringify(user.roles));
+            localStorage.setItem("token", user.token);
+            setEmail(user.email);
+            return user;
+        });
+    };
+
+    const signOut = () => {
+        setEmail(null);
+        localStorage.removeItem("email");
+        localStorage.removeItem("role");
+        localStorage.removeItem("token");
+    };
+
+    function isLoggedIn() {
+        return email !== null;
+    }
+
+    function isLoggedInAs(role: string[]) {
+       const roles: Array<string> = JSON.parse(localStorage.getItem("roles") || "[]");
+        return roles?.some((r) => role.includes(r)) || false;
+    }
+
+    const value = { email, signIn, signOut, isLoggedIn, isLoggedInAs };
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
