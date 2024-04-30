146b99824b6f71245aefda9ebbb30bbbf6881641
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-04-30 12:37:55 +0200

    CM-28 profil (not done) blocked

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import SignUp from "./components/SignUp";
+import UserProfile from "./components/UserProfile";
 
 export default function App() {
-  return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/settings" element={<UserProfile />} />
+            </Routes>
+        </div>
+    );
 }
