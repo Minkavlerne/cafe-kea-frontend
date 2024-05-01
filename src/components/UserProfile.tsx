4793a41ba0e25624192bf5a379ef1a2114b3c125
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-05-01 09:50:43 +0200

    CM-28 Userprofile

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,15 +1,18 @@
-import { useEffect, useState } from 'react';
-import { getUserByEmail } from '../services/apiFacade';
+import { useEffect, useState } from "react";
+import { getUserByEmail } from "../services/apiFacade";
 
-
-export default function UserProfile() { 
+export default function UserProfile() {
     useEffect(() => {
         getUserByEmail(localStorage.getItem("email")!).then((data) => console.log(data));
-    })
+    });
 
     return (
-        <div>
+        <div className="p-[100px]">
             <h1>User Profile</h1>
+            <p>Email</p>
+            <p>Password (button) change</p>
+            <p>Logout</p>
+            <p>Delete my account</p>
         </div>
     );
 }
