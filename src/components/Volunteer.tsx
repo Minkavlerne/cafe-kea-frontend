d4098082ff64bf2a38f27913d967eaa72afe02fd
sebbex1337 <sefort@hotmail.dk>
2024-05-13 11:11:17 +0200

    CM-78 volunteer page added

---
+++ src/components/Volunteer.tsx
@@ -0,0 +1,21 @@
+export default function Volunteer() {
+    return (
+        <div className="pt-[150px] px-[25rem] flex items-center justify-center">
+            <div>
+                <h1 className="text-center text-xl font-bold pb-4">Volunteer</h1>
+                <h2>
+                    This is KEA´s new coffee shop, by volunteers for volunteers. We are located at the ground floor of the new building at Guldbergsgade 29N. We are open to all students and staff at KEA. We offer a variety of coffee, tea,
+                    and snacks. We are open Monday to Friday from 8:00 to 14:00. We are looking for volunteers to help us run the coffee shop. If you are interested, please contact us at this e.mail: hello@kea.dk
+                </h2>
+                <br />
+                <p>
+                    Would you like to be our new volunteer? Click &#32;
+                    <a href="url" target="_blank" className="text-blue-600 underline">
+                        here
+                    </a>
+                    &#32; to fill out an application
+                </p>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/VolunteerPage.tsx
@@ -0,0 +1,9 @@
+import Volunteer from "../components/Volunteer";
+
+export default function VolunteerPage() {
+    return (
+        <div className="min-h-screen bg-background-kea">
+            <Volunteer />
+        </div>
+    );
+}
